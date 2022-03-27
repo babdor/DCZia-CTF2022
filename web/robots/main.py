58099c25aa95dc257031e2276254bae245ac73f2
Bill Weiss <bV@billweiss.net>
2022-03-27 14:04:06 -0400

    Add a goofy Bender party image

--- web/robots/main.py
+++ web/robots/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, make_response, render_template, request
+from flask import Flask, make_response, render_template, request, send_file
 import os
 
 app = Flask(__name__)
@@ -19,17 +19,45 @@ def robotstxt():
     response.headers['Content-Type'] = 'text/plain'
     return response
 
+# this is slightly jank
+# it returns True if the user is a robot
+# otherwise it returns the rendered 404 page
+# Sorry?
+def checkrobot(req):
+    ua = req.headers.get('User-Agent')
+    if ua == robotUA:
+        return True
+    else:
+        return make_response(render_template('smells-like-human.html'), 404)
+
+
 @app.route(robotpath)
 def robotflag():
-    ua = request.headers.get('User-Agent')
-    if ua == robotUA:
+    f = checkrobot(request)
+    if f == True:
         response = make_response(
             render_template('great-job-fellow-robot.txt')
             )
         response.headers['Content-Type'] = 'text/plain'
         return response
     else:
-        return make_response(render_template('smells-like-human.html'), 404)
+        return f
+
+@app.route('/JoinTheUprising')
+def robotuprising():
+    f = checkrobot(request)
+    if f == True:
+        response = make_response(
+                render_template('its-uprising-time.txt')
+            )
+        response.headers['Content-Type'] = 'text/plain'
+        return response
+    else:
+        return f
+
+@app.route('/HennaIsolationCatalystSycamoreUnranked')
+def robotGoodTime():
+    return render_template('HennaIsolationCatalystSycamoreUnranked.html')
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
